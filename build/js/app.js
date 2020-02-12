96006b5db4ed8f991a4732ea0b01c2f24cd96b3c
iiaii <jhun9409@naver.com>
2020-02-12 12:14:03 +0900

    s3의 이미지로 모두 변경

--- build/js/app.js
+++ build/js/app.js
@@ -877,7 +877,7 @@ $(function () {
     },
     autoplay: true,
     autoplayTimeout: 5000,
-    navText: ["<div id=\"btn-prev-owl\"><img src=\"./assets/image/arrow-left.png\"/></div>", "<div id=\"btn-next-owl\"><img src=\"./assets/image/arrow-right.png\"/></div>"]
+    navText: ["<div id=\"btn-prev-owl\"><img src=\"https://hakimwedding.s3.ap-northeast-2.amazonaws.com/image/arrow-left.png\"/></div>", "<div id=\"btn-next-owl\"><img src=\"https://hakimwedding.s3.ap-northeast-2.amazonaws.com/image/arrow-right.png\"/></div>"]
   })
 });
 // Preloader
